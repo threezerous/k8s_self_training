d770819dc5e440c82b638953b9044fc27e5fd1ee
Sushant Sonker <threezerous@users.noreply.github.com>
2019-06-06 19:27:21 -0400

    Update apiserver.sh

--- apiserver.sh
+++ apiserver.sh
@@ -2,7 +2,7 @@
 # Master [etc/kubernetes/apiserver]
 KUBE_API_ADDRESS="--address=0.0.0.0"
 KUBE_API_PORT="--port=8080"
-KEUBE_ETCD_SERVERS="--ectd-servers=http://kube-master:2379"
+KUBE_ETCD_SERVERS="--etcd-servers=http://kube-master:2379"
 KUBE_SERVICE_ADDRESSES="--service-cluster-ip-range=10.254.0.0/16"
 KUBE_ADMISSION_CONTROL=""
 KUBE_API_ARGS=""
