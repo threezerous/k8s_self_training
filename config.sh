f14037dac206ec3f98bb74e378d5b1ba72bdcdc7
Sushant Sonker <threezerous@users.noreply.github.com>
2019-06-06 19:31:36 -0400

    Update config.sh

--- config.sh
+++ config.sh
@@ -2,7 +2,7 @@
 # Master & Nodes
 # /etc/kubernetes/config
 
-KUBE_LOGTOSTDEER="--logtostdeer=true"
+KUBE_LOGTOSTDERR="--logtostderr=true"
 KUBE_LOG_LEVEL="--v=0"
 KUBE_ALLOW_PRIV="--allow_privileged=false"
 KUBE_MASTER="--master=http://kube-master:8080"
